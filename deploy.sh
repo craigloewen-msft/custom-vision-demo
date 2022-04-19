7e6fc7ed86ab008f9716e5d74c1c14811588ecf7
Craig Loewen <crloewen@microsoft.com>
2022-04-19 16:01:25 -0400

    Updated action

--- deploy.sh
+++ deploy.sh
@@ -12,12 +12,12 @@ echo Building
 npm run build
 
 # Commit on git
-git checkout --orphan gh-pages
+git checkout --orphan gh-pages2
 git --work-tree dist add --all
 git --work-tree dist commit -m "gh-pages"
 echo Pushing to pages
-git push origin HEAD:gh-pages --force
+git push origin HEAD:gh-pages2 --force
 rm -r ./dist
 git checkout -f main
-git branch -D gh-pages
+git branch -D gh-pages2
 echo Success!
